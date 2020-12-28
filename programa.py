847a09189ee9f08ac844b3538e4ef78eb4be5d7d
Claudia Jazmín Soria Saavedra <jazminsoria191998@gmail.com>
2020-12-28 13:36:46 +0100

    Ingreso impresión de noticia

--- programa.py
+++ programa.py
@@ -21,7 +21,12 @@ naive = naive_bayes(X_traincv, clases)
 
 #Decision Tree
 tree = decision_tree(X_traincv, clases)
+
 #Prediccion
-pred = tree.predict(X_traincv)
-print(pred)
+pred_tree = tree.predict(X_traincv[0])
+pred_naive = naive.predict(X_traincv[0])
+texto = string_doc(0, noticias)
+print("La noticia:\n", texto)
+print("\n según Decision Tree es de ", pred_tree)
+print("\n según Naive Bayes es de ", pred_naive)
 
