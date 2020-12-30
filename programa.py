d35405a943bb57a2ab386c2b4b16728a7919d862
claudiajazmin19 <jazminsoria191998@gmail.com>
2020-12-30 15:53:38 +0100

    commit

--- programa.py
+++ programa.py
@@ -1,12 +1,15 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from funciones import ingresar_noticias, texto_procesado, crea_clases, tfid, naive_bayes, decision_tree, string_doc, prediccion, ramdonforest, tfid_fit
 
+#TFIDF Vectorizer
 cv = TfidfVectorizer()
+
 #Noticias
 noticias = []
 ingresar_noticias("despoblación/*.txt", noticias)
 ingresar_noticias("no_despoblación/*.txt", noticias)
 
+#Noticias de prueba
 nuevas = []
 ingresar_noticias("unlabeled/*.txt", nuevas)
 
@@ -15,12 +18,14 @@ processed_text = []
 processed_text = texto_procesado(noticias)
 processed_text_nuevas = texto_procesado(nuevas)
 
-#Creación de clases
+#Creación de arreglo de clases
 clases = []
 clases = crea_clases(clases, processed_text)
 
-#TFIDF
+#TFIDF_noticias
 X_traincv = tfid(processed_text, cv)
+
+#TFIDF_prueba
 X_testcv = tfid_fit(processed_text_nuevas, cv)
 
 #Modelos
@@ -28,11 +33,12 @@ naive = naive_bayes(X_traincv, clases)
 tree = decision_tree(X_traincv, clases)
 random_forest = ramdonforest(X_traincv, clases)
 
-#Prediccion
+#Predicción
 pred_tree = prediccion(tree, X_testcv[0])
 pred_naive = prediccion(naive, X_testcv[0])
 pred_random = prediccion(random_forest, X_testcv[0])
 
+#Impresión de noticia
 noticia = string_doc(0, nuevas)
 
 print("La noticia:\n", noticia)
